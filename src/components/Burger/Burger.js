4d11878e0cf943b2675f4e4df1a13a9eb75788ce
Saumaya Mani Tripathi <manidecp@gmail.com>
2020-03-14 17:47:33 +0530

    Second commit with added ingredient state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,25 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredients/BurgerIngredient'
 
 const burger = (props) => {
+    //converting object to array
+    let transformIngredients = Object.keys(props.ingredients).map(keys => {
+        return [...Array(props.ingredients[keys])].map((_ , index) => {
+            return <BurgerIngredient key={keys + index} type={keys} />
+        });
+    }).reduce((arr , el) => {
+        return arr.concat(el)
+    } , []);
+
+    if(transformIngredients.length === 0) {
+        transformIngredients = <h5> Please add some elements </h5>;
+    }
+
+
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type={'bread-top'} />
-            <BurgerIngredient type={'cheese'} />
-            <BurgerIngredient type={'meat'} />
+                {transformIngredients}
             <BurgerIngredient type={'bread-bottom'} />
         </div>
     );
